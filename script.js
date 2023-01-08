e58e8303d025747c78efcc0cda60f7bfc6659221
Andreja <80786836+Andreja011@users.noreply.github.com>
2023-01-08 22:18:25 +0100

    Final solution for Sign Up form

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 "use strict";
+
+const check = function () {
+  if (
+    document.querySelector("#right-side-form-password").value ===
+    document.querySelector("#right-side-form-confirm-password").value
+  ) {
+    document.querySelector(`#right-side-form-password`).style.borderColor =
+      "green";
+    document.querySelector(`#right-side-form-password`).style.boxShadow =
+      "0px 0px 10px 5px var(--green)";
+    document.querySelector(
+      `#right-side-form-confirm-password`
+    ).style.borderColor = "green";
+    document.querySelector(
+      `#right-side-form-confirm-password`
+    ).style.boxShadow = "0px 0px 10px 5px var(--green)";
+    console.log("Matching");
+  } else {
+    document.querySelector(`#right-side-form-password`).style.borderColor =
+      "red";
+    document.querySelector(`#right-side-form-password`).style.boxShadow =
+      "0px 0px 10px 5px var(--red)";
+    document.querySelector(
+      `#right-side-form-confirm-password`
+    ).style.borderColor = "red";
+    document.querySelector(
+      `#right-side-form-confirm-password`
+    ).style.boxShadow = "0px 0px 10px 5px var(--red)";
+    console.log("Not Matching");
+  }
+};
